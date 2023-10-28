12ba4ce3e242d7ef5d748a5af7ddde33b98206e3
SH_ROOT <evgdol93@gmail.com>
2023-10-28 12:00:23 +0000

    upd load controller

--- app/load_switcher.c
+++ app/load_switcher.c
@@ -1,12 +1,17 @@
 #include "load_switcher.h"
 #include "adc.h"
 #include "stm32f4xx.h"
+#include <math.h>
 #include <stdbool.h>
 
 #define TIMEOUT_ON_MS 200
+#define TIMEOUT_CUR_LONGTERM_MS 4000
+#define CURRENT_FAULT_INSTANT_A 1.0f
+#define CURRENT_FAULT_LONGTERM_A 0.25f
 
 static bool enabled = false;
 static uint32_t timeout_on_ms = 0;
+static uint32_t timeout_cur_longterm_ms = 0;
 
 void load_switcher_init(void)
 {
@@ -43,10 +48,32 @@ int load_switcher_poll(uint32_t diff_ms)
 {
 	timeout_on_ms = timeout_on_ms > diff_ms ? timeout_on_ms - diff_ms : 0;
 
-	if(timeout_on_ms == 0 && !(GPIOE->IDR & GPIO_Pin_15)) // fault
+	if(timeout_on_ms == 0) // fault pin
 	{
-		load_switcher_off();
-		return enabled ? 1 : 0;
+		if(fabsf(adc_val.i_sns) >=  CURRENT_FAULT_LONGTERM_A)
+		{
+			timeout_cur_longterm_ms += diff_ms;
+		}
+		else
+		{
+			timeout_cur_longterm_ms = 0;
+		}
+
+		if(!(GPIOE->IDR & GPIO_Pin_15))
+		{
+			load_switcher_off();
+			return enabled ? 1 : 0;
+		}
+		if(fabsf(adc_val.i_sns) >= CURRENT_FAULT_INSTANT_A)
+		{
+			load_switcher_off();
+			return enabled ? 2 : 0;
+		}
+		if(timeout_cur_longterm_ms >= TIMEOUT_CUR_LONGTERM_MS)
+		{
+			load_switcher_off();
+			return enabled ? 3 : 0;
+		}
 	}
 	return 0;
 }
